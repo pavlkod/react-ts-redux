cbaffab788a6203e2e7eeca9823c54998bf6e1be
Pavel.K <pavelweb74@gmail.com>
2021-10-11 23:18:15 +0500

    Add user reducer

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,20 @@
+import { UserState, UserAction, UserActionTypes } from "../../types/user";
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+export const userReducer = (state = initialState, action: UserAction): UserState => {
+  switch (action.type) {
+    case UserActionTypes.FETCH_USERS:
+      return { ...state, loading: true, error: null };
+    case UserActionTypes.FETCH_USERS_SUCCESS:
+      return { ...state, loading: false, error: null, users: action.payload };
+    case UserActionTypes.FETCH_USERS_ERROR:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
