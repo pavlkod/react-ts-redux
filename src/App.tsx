b8073ba1a6fdf84decc951689e8b884daaef1451
Pavel.K <pavelweb74@gmail.com>
2021-10-12 20:57:26 +0500

    Add user list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import UserList from "./components/UserList";
 
 function App() {
-  return <div className="App">It work</div>;
+  return (
+    <div className="App">
+      It work
+      <UserList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UserList.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { fetchUsers } from "../store/action-creators/user";
+
+const UserList: FC = () => {
+  const { users, error, loading } = useTypedSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+  return (
+    <div>
+      {users.map(user => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
