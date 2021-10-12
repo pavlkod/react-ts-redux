e25c5b089343bcf2a82c3d9ad7d08654d046ddd0
Pavel.K <pavelweb74@gmail.com>
2021-10-12 20:57:07 +0500

    Add fetchUsers action creator

---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { UserAction, UserActionTypes } from "../../types/user";
+
+export const fetchUsers = () => async (dispatch: Dispatch<UserAction>) => {
+  try {
+    dispatch({ type: UserActionTypes.FETCH_USERS });
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    dispatch({ type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data });
+  } catch (e) {
+    dispatch({ type: UserActionTypes.FETCH_USERS_ERROR, payload: "Can't get users" });
+  }
+};
