f4120233baf39ada7a577fe7c475b1e8d6f0bf58
Pavel.K <pavelweb74@gmail.com>
2021-10-13 23:19:28 +0500

    Add types for todoReducer

---
+++ src/types/todo.ts
@@ -0,0 +1,32 @@
+export interface TodoState {
+  todo: any[];
+  loading: boolean;
+  error: null | string;
+  page: number;
+  limit: number;
+}
+
+export enum TodoActionsType {
+  FETCH_TODO = "FETCH_TODO",
+  FETCH_TODO_SUCCESS = "FETCH_TODO_SUCCESS",
+  FETCH_TODO_ERROR = "FETCH_TODO_ERROR",
+  SET_TODO_PAGE = "SET_TODO_PAGE",
+}
+
+interface FetchTodoAction {
+  type: TodoActionsType.FETCH_TODO;
+}
+interface FetchTodoSuccessAction {
+  type: TodoActionsType.FETCH_TODO_SUCCESS;
+  payload: any[];
+}
+interface FetchTodoErrorAction {
+  type: TodoActionsType.FETCH_TODO_ERROR;
+  payload: string;
+}
+interface SetTodoPageAction {
+  type: TodoActionsType.SET_TODO_PAGE;
+  payload: number;
+}
+
+export type TodoAction = FetchTodoAction | FetchTodoErrorAction | FetchTodoSuccessAction | SetTodoPageAction;
